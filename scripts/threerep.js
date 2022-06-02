37066a9a499b5fc8b603aa28aa3443e8b39cee1f
Jan Erik Hanssen <jhanssen@gmail.com>
2022-06-01 20:38:41 -0700

    replace script

---
+++ scripts/threerep.js
@@ -0,0 +1,15 @@
+const fs = require("fs/promises");
+
+const inp = process.argv.slice(2)[0];
+const outp = process.argv.slice(2)[1];
+
+(async function() {
+    let d = await fs.readFile(inp, "utf8");
+    d = d.replaceAll("three.", "");
+    await fs.writeFile(outp, d, "utf8");
+})().then(() => {
+    process.exit(0);
+}).catch(e => {
+    console.error(e);
+    process.exit(1);
+});
